916268088123ecd88bed52fbfc43d5bbf1df1d68
Vittorio Polto <vittorio.polto@axant.it>
2017-08-30 16:11:56 +0200

    Increased version to 0.9.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ except IOError:
 
 setup(
     name='tgapp-mailtemplates',
-    version='0.8.0',
+    version='0.9.0',
     description='Email template management for web applications',
     long_description=README,
     author='Marco Bosio',
