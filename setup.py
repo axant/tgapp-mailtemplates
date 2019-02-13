14a1e7f398789db0ec1856c50f66e91a357e2f30
Vittorio Polto <vittorio.polto@axant.it>
2019-02-13 14:59:57 +0100

    Upgrade version 0.13.6

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ except IOError:
 
 setup(
     name='tgapp-mailtemplates',
-    version='0.13.5',
+    version='0.13.6',
     description='Email template management for web applications',
     long_description=README,
     author='Marco Bosio',
